75a02d3b891a2c1f5de609f3e475f15a07964454
Christopher Nabors <PillarDD@Gmail.com>
2012-07-10 05:46:29 -0400

    Finished the alpha version of the US_Natality project

--- data/js/us_natality.js
+++ data/js/us_natality.js
@@ -10,71 +10,16 @@
 // Northeast
 //      Connecticut, Maine, Massachusetts, New Hampshire, New Jersey,
 //      New York, Pennsylvania, Rhode Island, Vermont
-var northeast = [
-        ['Connecticut', 0],
-        ['Maine', 0],
-        ['Massachusetts', 0],
-        ['New Hampshire', 0],
-        ['New Jersey', 0],
-        ['New York', 0],
-        ['Pennsylvania', 0],
-        ['Rhode Island', 0],
-        ['Vermont', 0]];
 // Midwest
 //      Illinois, Indiana, Iowa, Kansas, Michigan, Minnesota, Missouri,
 //      Nebraska, North Dakota, Ohio, South Dakota, Wisconsin
-var midwest = [
-        ['Illinois', 0],
-        ['Indiana', 0],
-        ['Iowa', 0],
-        ['Kansas', 0],
-        ['Michigan', 0],
-        ['Minnesota', 0],
-        ['Missorui', 0],
-        ['Nebraska', 0],
-        ['North Dakota', 0],
-        ['Ohio', 0],
-        ['South Dakota', 0],
-        ['Wisconsin', 0]];
 // South
 //      Alabama, Arkansas, Delaware, District of Columbia, Florida, Georgia,
 //      Kentucky, Louisiana, Maryland, Mississippi, North Carolina,
 //      Oklahoma, South Carolina, Tennessee, Texas, Viriginia, West Virginia
-var south = [
-        ['Alabama', 0],
-        ['Arkansas', 0],
-        ['Delaware', 0],
-        ['District of Columbia', 0],
-        ['Florida', 0],
-        ['Georgia', 0],
-        ['Kentucky', 0],
-        ['Louisiana', 0],
-        ['Maryland', 0],
-        ['Mississippi', 0],
-        ['North Carolina', 0],
-        ['Oklahoma', 0],
-        ['South Carolina', 0],
-        ['Tennessee', 0],
-        ['Texas', 0],
-        ['Virginia', 0],
-        ['West Virginia', 0]];
 // West
 //      Alaska, Arizona, California, Colorado, Hawaii, Idaho, Montana,
 //      Nevada, New Mexico, Oregon, Utah, Washington, Wyoming
-var west = [
-        ['Alaska', 0],
-        ['Arizona', 0],
-        ['California', 0],
-        ['Colorado', 0],
-        ['Hawaii', 0],
-        ['Idaho', 0],
-        ['Montana', 0],
-        ['Nevada', 0],
-        ['New Mexico', 0],
-        ['Oregon', 0],
-        ['Utah', 0],
-        ['Washington', 0],
-        ['Wyoming', 0]];
 
 /******************************************************************************\
 | Global Variables                                                             |
@@ -96,6 +41,9 @@ var color_holder;
 var return_text;
     // This variable is used as a palceholder when the states are formed. It
     //      contains the hover text.
+var current_state_data = [10,10];
+    // This variable will hold the current male-female numbers for the state pie
+    //      chart.
 /******************************************************************************\
 | Global Functions                                                             |
 \******************************************************************************/
@@ -164,6 +112,7 @@ function map_init(){
                 return color_holder;
                 })
             .style("stroke", "black")
+            .style("cursor", "pointer")
             .attr("d", path)
             .append("svg:title")
             .text(function(d){ 
@@ -174,7 +123,54 @@ function map_init(){
                 }
                 return return_text;
                 });
+        states.selectAll("path").on("click", function(d){
+            add_middle_content();
+
+            var state_name = d.properties.name;
+           
+            $('main_container_state_pie_title').innerHTML = state_name;
+
+            // Clear the div in preparation for the new pie chart.
+            $('main_container_state_pie').innerHTML = "";
+            
+            // Load in the new data - [male#, female#]
+            for(var i=0; i<state_births_array['2007-2009'].length; i++){
+                if(state_name === state_births_array['2007-2009'][i]['state']){
+                    var data_array = [state_births_array['2007-2009'][i]['male'],
+                        state_births_array['2007-2009'][i]['female']];
+                    create_state_pie(data_array);
+                    $('male_count_body').innerHTML = (state_births_array['2007-2009'][i]['male']);
+                    $('female_count_body').innerHTML = (state_births_array['2007-2009'][i]['female']);
+                }
+            }
+        });
     });
+}
+function add_middle_content(){
+    $('main_container_national_pie').fade(0); 
+    $('main_container_northeast_pie').fade(0); 
+    $('main_container_west_pie').fade(0); 
+    $('main_container_midwest_pie').fade(0); 
+    $('main_container_south_pie').fade(0); 
+    $('main_container_state_pie').fade(1);
+    $('main_container_state_pie_title').fade(1);
+    $('male_count_container').fade(1);
+    $('female_count_container').fade(1);
+    $('click_to_return').fade(1);
+    $('bottom_content_main_container').setStyle('cursor','pointer');
+}
+function remove_middle_content(){
+    $('main_container_national_pie').fade(1); 
+    $('main_container_northeast_pie').fade(1); 
+    $('main_container_west_pie').fade(1); 
+    $('main_container_midwest_pie').fade(1); 
+    $('main_container_south_pie').fade(1);
+    $('main_container_state_pie').fade(0);
+    $('main_container_state_pie_title').fade(0);
+    $('male_count_container').fade(0);
+    $('female_count_container').fade(0);
+    $('click_to_return').fade(0);
+    $('bottom_content_main_container').setStyle('cursor','auto');
 }
 function populate_names(){
     // www.ssa.gov/oact/babynames/#ht=1
@@ -208,7 +204,7 @@ function populate_top_ten(){
         var temp_content = "<ul>";
         for(var i=0;i<10;i++){
             temp_content += ("<li><div id='" +
-                top_ten_array[i][1] + "_box'>" +
+                top_ten_array[i][1] + "_box' class='state_box'>" +
                 top_ten_array[i][1] + "<br /><div class='state_number'>" + 
                 top_ten_array[i][0] + "</div></div></li>");
         }
@@ -216,52 +212,92 @@ function populate_top_ten(){
         $('bottom_content_left_container_body').innerHTML = temp_content;
         
         $('bottom_content_main_container').addEvent('click', function(){
-            $('main_container_national_pie').fade(1); 
-            $('main_container_northeast_pie').fade(1); 
-            $('main_container_west_pie').fade(1); 
-            $('main_container_midwest_pie').fade(1); 
-            $('main_container_south_pie').fade(1);
-            $('main_container_state_pie').fade(0);
+            remove_middle_content();
         });
         
         for(var i=0;i<10;i++){
             var temp_place = '';
             temp_place = top_ten_array[i][1] + '_box';
-            console.log('temp_place_array: ' + temp_place); 
-
             add_event_to_state(temp_place);
         }
     });
     
 }
-function add_event_to_state(state){
-    $(state).addEvent('click', function(){
-        $('main_container_national_pie').fade(0); 
-        $('main_container_northeast_pie').fade(0); 
-        $('main_container_west_pie').fade(0); 
-        $('main_container_midwest_pie').fade(0); 
-        $('main_container_south_pie').fade(0); 
-        $('main_container_state_pie').fade(1);
-        $('main_container_state_pie').innerHTML = ('Currently Loaded: ' +
-            state);
-    });
-}
 function populate_full_list(){
     var full_list_array = [];
-
+    
     d3.json("data/json/state_births.json", function(json){
-        $('bottom_content_right_container_body').innerHTML = "<ul>"
         for(var i=0;i<json['2007-2009'].length;i++){
-            $('bottom_content_right_container_body').innerHTML +=
-                "<li>" + json['2007-2009'][i]['state'] + "<ul><li>" +
-                    json['2007-2009'][i]['births'] + "</li></ul></li>";
+            full_list_array[i] = [json['2007-2009'][i]['births'],
+                json['2007-2009'][i]['state']];
         }
-        $('bottom_content_right_container_body').innerHTML += "</ul>"
+        
+        var temp_content = "<ul>";
+        for(var i=0;i<full_list_array.length;i++){
+            temp_content += ("<li><div id='" +
+                full_list_array[i][1] + "_lst' class='state_box'>" +
+                full_list_array[i][1] + "<br /><div class='state_number'>" +
+                full_list_array[i][0] + "</div></div></li>");
+        }
+        temp_content += "</ul>";
+        $('bottom_content_right_container_body').innerHTML = temp_content;
+
+        for(var i=0;i<full_list_array.length;i++){
+            var temp_place = '';
+            temp_place = full_list_array[i][1] + '_lst';
+            add_event_to_state(temp_place);
+        }
     });
 }
+function add_event_to_state(state){
+    $(state).addEvent('click', function(){
+        add_middle_content();
 
+        // Ex - Florida_box -> Florida
+        var state_name = state.substring(0, state.length - 4);
+       
+        $('main_container_state_pie_title').innerHTML = state_name;
+
+        // Clear the div in preparation for the new pie chart.
+        $('main_container_state_pie').innerHTML = "";
+        
+        // Load in the new data - [male#, female#]
+        for(var i=0; i<state_births_array['2007-2009'].length; i++){
+            if(state_name === state_births_array['2007-2009'][i]['state']){
+                var data_array = [state_births_array['2007-2009'][i]['male'],
+                    state_births_array['2007-2009'][i]['female']];
+                create_state_pie(data_array);
+                $('male_count_body').innerHTML = (state_births_array['2007-2009'][i]['male']);
+                $('female_count_body').innerHTML = (state_births_array['2007-2009'][i]['female']);
+            }
+        }
+    });
+}
+function create_state_pie(state_data){
+    var width = 550;
+    var height = 550;
+    var w = (width - 100),
+        h = (height - 20),
+        r = Math.min(w, h) / 2,
+        color = d3.scale.category20()
+            .range(['#17becf', '#f7b6d2']),
+        donut = d3.layout.pie().sort(null),
+        arc = d3.svg.arc().innerRadius(0).outerRadius(r - 10);
+    
+    var svg = d3.select('#main_container_state_pie').append("svg:svg")
+        .attr("width", w)
+        .attr("height", h)
+        .append("svg:g")
+        .attr("transform", "translate(" + (w/2) + "," + (h/2) + ")");
+    
+    var arcs = svg.selectAll("path")
+        .data(donut(state_data))
+        .enter().append("svg:path")
+        .attr("fill", function(d, i){ return color(i); })
+        .attr("d", arc)
+        .each(function(d){ this._current = d; });
+}
 function default_region_charts(){
-    //$('#main_container_state_pie').fade(0);
     var national_color = d3.scale
         .linear()
         .domain([6110, 527020])
@@ -287,6 +323,8 @@ function default_region_charts(){
         .domain([9040, 401977])
         .range(['#E5F5F9','#2CA25F']);
 
+    // Create the Blank State Chart
+    create_state_pie(current_state_data);
 
     // United States (Large Circle)
     d3.csv("data/json/national_births.csv", function(csv){
